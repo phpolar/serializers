898b2b9b375c213d867390546e7ac724cb5b794f
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2025-06-27 22:41:45 -0400

    feat: initial commit
    Signed-off-by: Eric Fortmeyer <e.fortmeyer01@gmail.com>

---
+++ src/JsonSerializer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Phpolar\Serializers;
+
+use PhpContrib\Serializer\SerializerInterface;
+
+/**
+ * Converts data into the JSON data-interchange format.
+ */
+final class JsonSerializer implements SerializerInterface
+{
+    /**
+     * @param int<1,max> $depth
+     * @param callable(mixed $data): string | null $failureHandler
+     */
+    public function __construct(private int $flags = 0, private int $depth = 512, private $failureHandler = null)
+    {
+    }
+
+    public function serialize(mixed $data): string
+    {
+        $result = json_encode($data, $this->flags, $this->depth);
+
+        return $result === false ? $this->handleFailure($data) : $result;
+    }
+
+    private function handleFailure(mixed $data): string
+    {
+        return $this->failureHandler !== null ? ($this->failureHandler)($data) : "";
+    }
+}
